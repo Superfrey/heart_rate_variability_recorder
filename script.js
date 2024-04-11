c0eec5c92e88b22bccde8c647fc8bdc794dc80fe
Jonas Schaarup <60611191+Superfrey@users.noreply.github.com>
2024-04-11 15:50:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,12 @@ function calculateResults() {
     // Calculate standard deviation
     const sd = Math.sqrt(intervals.reduce((sq, n) => sq + Math.pow(n - meanInterval, 2), 0) / intervals.length);
     
+    // Calculate RMSSD
+    const successiveDifferences = intervals.slice(1).map((interval, index) => Math.pow(interval - intervals[index], 2));
+    const rmssd = Math.sqrt(successiveDifferences.reduce((a, b) => a + b, 0) / successiveDifferences.length);
+
     // Update webpage with results
-    document.getElementById('results').innerHTML = `Mean Interval: ${meanInterval.toFixed(2)} ms<br>Standard Deviation: ${sd.toFixed(2)} ms`;
+    document.getElementById('results').innerHTML = `Mean Interval: ${meanInterval.toFixed(2)} ms<br>Standard deviation of heartbeat intervals: ${sd.toFixed(2)} ms<br>RMSSD: ${rmssd.toFixed(2)} ms`;
 }
+
 
